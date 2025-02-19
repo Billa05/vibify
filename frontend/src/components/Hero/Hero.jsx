54ac6e1de738af8357e961e46942b16e880f89fe
billa05 <workinguse5@gmail.com>
2025-02-20 00:04:48 +0530

    link

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -3,17 +3,22 @@ import "./hero.css";
 import spotify_ui from "../../../assets/spotify_ui.png";
 import spotify from "../../../assets/spotify_dark.png";
 
+
+const handleClick = () => {
+  window.location.href = "/mood";
+};
+
 function Hero() {
   return (
     <div className="hero">
       <div>
         <h1>Welcome</h1>
         <h2>vibify is a spotify playlist generator </h2>
-        <button className="minimal-btn">sign in<img src = {spotify}/></button>
+        <button className="minimal-btn">sign in<img src={spotify} /></button>
         <p>or</p>
-        <button className="minimal-btn-dark">continue as a guest</button>      
+        <button onClick={handleClick} className="minimal-btn-dark">continue as a guest</button>
       </div>
-      <img src={spotify_ui}/>
+      <img src={spotify_ui} />
     </div>
   );
 }
